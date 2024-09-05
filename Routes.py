fe566b843a5dce615ff7732be234f83b5e8f052b
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-09-06 10:24:32 +1200

    added pending pages + new table

--- Routes.py
+++ Routes.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect, url_for
+from flask import Flask, request, render_template, redirect
 from werkzeug.utils import secure_filename
 import sqlite3
 import os
@@ -34,14 +34,89 @@ def passwordpage():
     return render_template('password.html', color=color)
 
 
-@app.route('/pending')  # Route to the pending page
+@app.route('/pending', methods=["GET", "POST"])  # Route to the pending page
 def pendingpage():
     color = "#246eff"
     conn = sqlite3.connect("Webdatabase.db")
     cur = conn.cursor()
     cur.execute("SELECT * FROM Pending")
     trashes = cur.fetchall()
+    if request.method == "POST":
+        if "go" in request.form:
+            pass
     return render_template('pending.html', color=color, trashes=trashes)
+
+
+@app.route('/penditem/<int:id>', methods=["GET", "POST"])  # Route to the pending page
+def pendingitempage(id):
+    conn = sqlite3.connect("Webdatabase.db")
+    cur = conn.cursor()
+    # Selects all info of a row where the ID equals to the page ID
+    cur.execute("SELECT * FROM Pending WHERE id=?", (id,))
+    trash = cur.fetchone()
+    cur.execute("SELECT id FROM Pending ORDER BY ID DESC")
+    limitbig = cur.fetchone()
+    idp = id+1
+    idm = id-1
+    idp10 = id+10
+    idm10 = id-10
+    hideprevall = " "
+    hideforwall = " "
+    hideforw = " "
+    hideprev = " "
+    print(request.method)
+    if request.method == "POST":
+        if "add" in request.form:
+            cur.execute("""INSERT INTO Trash (name, description, instructions,
+                    bins, rrr, image)VALUES
+                    ((SELECT name FROM Pending WHERE id=?),
+                    (SELECT description FROM Pending WHERE id=?),
+                    (SELECT instructions FROM Pending WHERE id=?),
+                    (SELECT bins FROM Pending WHERE id=?),
+                    (SELECT rrr FROM Pending WHERE id=?),
+                    (SELECT image FROM Pending WHERE id=?))""", (id, id, id,
+                                                                 id, id, id,))
+            cur.execute("""DELETE FROM Pending WHERE id=?""", (id,))
+            conn.commit()
+            return redirect("http://127.0.0.1:5000/pending")
+        if "delete" in request.form:
+            cur.execute("""DELETE FROM Pending WHERE id=?""", (id,))
+            conn.commit()
+            return redirect("http://127.0.0.1:5000")
+    # Limits the user for going past page beyond the num of items
+    if id == limitbig[0] or id >= limitbig[0]:
+        id = limitbig[0]
+        idp = id
+        idp10 = id
+        hideforw = "hidden"
+    # Limits the user for going past page beyond the num of items
+    if id == 1 or id <= 1:
+        id = 1
+        idm = id
+        idm10 = id
+        hideprev = "hidden"
+    if id <= 10:
+        hideprevall = "hidden"
+    if id+9 >= limitbig[0]:
+        hideforwall = "hidden"
+    cur = conn.cursor()
+    # Takes the condition number for color
+    cur.execute("SELECT bins FROM Pending WHERE id=?", (id,))
+    bins = cur.fetchone()
+    color = "#246eff"
+    # Below conditions checks number for unique colors
+    if bins == (1,):
+        color = "#246eff"
+    if bins == (2,) or bins == (6,):
+        color = "#ff495c"
+    if bins == (3,) or bins == (5,) or bins == (7,) or bins == (8,):
+        color = "#f7cb15"
+    if bins == (4,):
+        color = "#3ddc97"
+    return render_template('pendingitem.html', trash=trash, idp=idp, idm=idm,
+                           idp10=idp10, idm10=idm10, hideprev=hideprev,
+                           hideforw=hideforw, hideprevall=hideprevall,
+                           hideforwall=hideforwall, color=color)
 
 
 def allowed_file(filename):
